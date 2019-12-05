eded56cc9e51040cbf52b8311d28ed6e33e866f4
Hoi Fong Chan <22822185+chancie86@users.noreply.github.com>
2019-12-05 20:41:32 +0000

    Day 4 Part 2

--- Day4/Day4.ps1
+++ Day4/Day4.ps1
@@ -34,31 +34,67 @@ function HasDoubleDigits
         $Number
     )
 
-    $Number.ToString() -match "([0-9])\1+"
+    $Number.ToString() -match "(\d)(\1)"
 }
 
-$validNumbers = New-Object -TypeName System.Collections.ArrayList
-
-$biggestNumber = $range[$range.Length-1]
-
-for ($number = (MakeAscendingNumbers -Number $range[0]); $number -le $biggestNumber; $number++)
+function HasDoubleDigitsExact
 {
-    if (($number % 10) -eq 0)
-    {
-        $number = MakeAscendingNumbers -Number $number
-        if ($number -gt $biggestNumber)
-        {
-            break
-        }
-    }
+    param(
+        [int]
+        $Number
+    )
 
-    if (-not(HasDoubleDigits -Number $number))
-    {
-        continue
-    }
-    
-    $validNumbers.Add($number) | Out-Null
-    Write-Verbose $number
+    $regex = [regex]"(\d)\1+"
+    $matches = $regex.Matches($Number.ToString())
+    ($matches | ? { $_.Value.Length -eq 2 }) -ne $null
 }
 
-return $validNumbers.Count
+function FindMatch
+{
+    param (
+        [int[]]
+        $Range,
+
+        [switch]
+        $HasDoubleDigitsExact
+    )
+
+    $validNumbers = New-Object -TypeName System.Collections.ArrayList
+
+    $biggestNumber = $Range[$Range.Length-1]
+
+    for ($number = (MakeAscendingNumbers -Number $Range[0]); $number -le $biggestNumber; $number++)
+    {
+        if (($number % 10) -eq 0)
+        {
+            $number = MakeAscendingNumbers -Number $number
+            if ($number -gt $biggestNumber)
+            {
+                break
+            }
+        }
+
+        if ($HasDoubleDigitsExact)
+        {
+            if (-not(HasDoubleDigitsExact -Number $number))
+            {
+                continue
+            }
+        }
+        elseif (-not(HasDoubleDigits -Number $number))
+        {
+            continue
+        }
+    
+        $validNumbers.Add($number) | Out-Null
+        Write-Verbose $number
+    }
+
+    $validNumbers
+}
+
+$answer = FindMatch -Range $range
+Write-Host "Part 1: $($answer.Count)"
+
+$answer = FindMatch -Range $answer -HasDoubleDigitsExact
+Write-Host "Part 2: $($answer.Count)"
