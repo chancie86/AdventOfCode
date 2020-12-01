412782546fff3fd8bba7e6310c1cab7834b0ea0d
Hoi Chan <22822185+chancie86@users.noreply.github.com>
2020-12-01 11:48:52 +0000

    Day1

---
+++ 2020/Day 1/Day1/Day1/Part1Task.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    public class Part1Task
+    {
+        private int? _result;
+
+        public Part1Task(IList<int> data)
+        {
+            Data = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+        public IList<int> Data { get; }
+
+        public void Run()
+        {
+            _result = FindMatches();
+        }
+
+        private int FindMatches()
+        {
+            for (var i = 0; i < Data.Count; i++)
+            {
+                for (var j = i; j < Data.Count; j++)
+                {
+                    var a = Data[i];
+                    var b = Data[j];
+
+                    if (CheckSum(a, b))
+                    {
+                        return a * b;
+                    }
+                }
+            }
+
+            throw new Exception("Couldn't find expected number of matches");
+        }
+
+        protected static bool CheckSum(int a, int b)
+        {
+            return (a + b) == 2020;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Part1Task)}: {_result.Value}";
+        }
+    }
+}
---
+++ 2020/Day 1/Day1/Day1/Part2Task.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    public class Part2Task
+    {
+        private int? _result;
+
+        public Part2Task(IList<int> data)
+        {
+            Data = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+        public IList<int> Data { get; }
+
+        public void Run()
+        {
+            _result = FindMatches();
+        }
+
+        private int FindMatches()
+        {
+            for (var i = 0; i < Data.Count; i++)
+            {
+                for (var j = i; j < Data.Count; j++)
+                {
+                    for (var k = j; k < Data.Count; k++)
+                    {
+                        var a = Data[i];
+                        var b = Data[j];
+                        var c = Data[k];
+
+                        if (CheckSum(a, b, c))
+                        {
+                            return a * b * c;
+                        }
+                    }
+                }
+            }
+
+            throw new Exception("Couldn't find expected number of matches");
+        }
+
+        protected static bool CheckSum(int a, int b, int c)
+        {
+            return (a + b + c) == 2020;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Part2Task)}: {_result.Value}";
+        }
+    }
+}
