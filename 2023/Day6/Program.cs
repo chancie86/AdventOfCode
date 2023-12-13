9b55e19d991a64f64ed0dfd8b00d2f40173216ef
Hoi Chan <96242851+hoi-salve@users.noreply.github.com>
2023-12-13 23:23:00 +0000

    AoC Day 6

---
+++ 2023/Day6/Program.cs
@@ -0,0 +1,78 @@
+﻿using Day6;
+
+await Part1();
+await Part2();
+
+public static partial class Program
+{
+    private static async Task<IList<Race>> ReadInput(bool ignoreSpaces)
+    {
+        var lines = await File.ReadAllLinesAsync("input.txt");
+        
+        if (ignoreSpaces)
+        {
+            var time = long.Parse(lines[0].Replace(" ", string.Empty).Substring("Time:".Length));
+            var distance = long.Parse(lines[1].Replace(" ", string.Empty).Substring("Distance:".Length));
+
+            return new[]
+            {
+                new Race(time, distance)
+            };
+        }
+
+        var timesSplit = lines[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var distanceSplit = lines[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        var races = new List<Race>();
+
+        for (var i = 1; i < timesSplit.Length; i++)
+        {
+            var time = int.Parse(timesSplit[i]);
+            var distance = int.Parse(distanceSplit[i]);
+
+            var race = new Race(time, distance);
+            races.Add(race);
+        }
+
+        return races;
+    }
+
+    private static int NumberOfWaysOfBeatingRecord(Race race)
+    {
+        var count = 0;
+
+        for (var chargeTime = 1; chargeTime < race.Time; chargeTime++)
+        {
+            var speed = chargeTime;
+            var distanceAchieved = speed * (race.Time - chargeTime);
+
+            if (distanceAchieved > race.RecordDistance)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static async Task Part1()
+    {
+        var races = await ReadInput(false);
+        var beatingScores = races.Select(NumberOfWaysOfBeatingRecord);
+
+        var part1 = 1;
+
+        foreach (var beatingScore in beatingScores)
+        {
+            part1 *= beatingScore;
+        }
+
+        Console.WriteLine($"Part 1: {part1}");
+    }
+
+    private static async Task Part2()
+    {
+        var race = (await ReadInput(true)).Single();
+        Console.WriteLine($"Part 2: {NumberOfWaysOfBeatingRecord(race)}");
+    }
+}
---
+++ 2023/Day6/Race.cs
@@ -0,0 +1,14 @@
+﻿namespace Day6
+{
+    public class Race
+    {
+        public Race(long time, long recordDistance)
+        {
+            Time = time;
+            RecordDistance = recordDistance;
+        }
+
+        public long Time { get; }
+        public long RecordDistance { get; }
+    }
+}
