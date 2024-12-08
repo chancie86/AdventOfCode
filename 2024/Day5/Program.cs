9495c9e35328eb0a4192dba794a58f1b3afa3f16
Hoi Fong Chan <22822185+chancie86@users.noreply.github.com>
2024-12-08 16:36:06 +0000

    AoC Day 5

---
+++ 2024/Day5/Program.cs
@@ -0,0 +1,123 @@
+﻿using Day5;
+
+var mode = Mode.Production;
+string inputPath;
+
+switch (mode)
+{
+    case Mode.Example:
+        Debug = true;
+        inputPath = "example.txt";
+        break;
+    case Mode.Test:
+        Debug = true;
+        inputPath = "tests.txt";
+        break;
+    case Mode.Production:
+        Debug = false;
+        inputPath = "input.txt";
+        break;
+    default:
+        throw new Exception("Invalid mode");
+}
+
+var input = await File.ReadAllLinesAsync(inputPath);
+var rules = new List<Rule>();
+var updates = new List<List<int>>();
+
+// Read the rules
+int i;
+for (i = 0; i < input.Length;)
+{
+    var line = input[i++];
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        break;
+    }
+
+    rules.Add(new Rule(line));
+}
+
+
+for (; i < input.Length; i++)
+{
+    var line = input[i];
+    var update = line.Split(',').Select(int.Parse).ToList();
+    updates.Add(update);
+}
+
+//Part1(rules, updates);
+Part2(rules, updates);
+
+public partial class Program
+{
+    private static bool Debug;
+
+    public static void Part1(List<Rule> rules, List<List<int>> updates)
+    {
+        var result = 0;
+
+        foreach (var update in updates)
+        {
+            if (rules.All(r => r.Validate(update)))
+            {
+                // Get middle number
+                var middleIndex = update.Count / 2;
+                result += update[middleIndex];
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    public static void Part2(List<Rule> rules, List<List<int>> updates)
+    {
+        var result = 0;
+
+        foreach (var update in updates)
+        {
+            var fixedUpdate = false;
+            bool changed;
+
+            do
+            {
+                changed = false;
+
+                foreach (var rule in rules)
+                {
+                    if (!rule.Validate(update))
+                    {
+                        fixedUpdate = true;
+                        changed = true;
+
+                        // Fix the update
+                        var first = update.IndexOf(rule.After);
+                        var second = update.IndexOf(rule.Before);
+
+                        // Put second before first
+                        update.RemoveAt(second);
+                        update.Insert(first, rule.Before);
+
+                        break;
+                    }
+                }
+            } while (changed);
+
+            if (fixedUpdate)
+            {
+                // Get middle number
+                var middleIndex = update.Count / 2;
+                result += update[middleIndex];
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    private enum Mode
+    {
+        Example,
+        Test,
+        Production
+    }
+}
---
+++ 2024/Day5/Rule.cs
@@ -0,0 +1,59 @@
+﻿namespace Day5
+{
+    public class Rule
+    {
+        public Rule(string line)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                throw new ArgumentException($"Invalid input: '{line}'", nameof(line));
+            }
+
+            var split = line.Split('|', StringSplitOptions.RemoveEmptyEntries);
+            Before = int.Parse(split[0]);
+            After = int.Parse(split[1]);
+        }
+
+        public int Before { get; set; }
+        public int After { get; set; }
+
+        public bool Validate(IEnumerable<int> data)
+        {
+            var foundAfter = false;
+
+            foreach (var datum in data)
+            {
+                foundAfter |= datum == After;
+
+                if (foundAfter
+                    && datum == Before)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool Validate(int a, int b)
+        {
+            if (!Match(a, b))
+            {
+                return true;
+            }
+
+            if (a == Before)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Match(int a, int b)
+        {
+            return (a == Before || a == After)
+                && (b == Before || b == After);
+        }
+    }
+}
